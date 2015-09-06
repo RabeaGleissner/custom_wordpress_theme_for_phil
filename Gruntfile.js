09ea90b30bbf996a14702944edea3bbdb7d1ffdb
Rabea Gleissner <rabeagleissner@gmail.com>
2015-09-06 20:17:12 +0100

    adds autoprefixer and production sass task

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,19 +25,55 @@ grunt.initConfig({
       }
     },
     sass: {
+        production : {
+          files : [
+            {
+              src : ['**/*.scss', '!**/_*.scss'],
+              cwd : 'assets/scss',
+              dest : 'assets/css',
+              ext : '.css',
+              expand : true
+            }
+          ],
+          options : {
+            style : 'compressed'
+          }
+        },
         dev: {
-            files: {
-                'assets/css/styles.css': 'assets/scss/styles.scss'
-            },
+            files : [
+              {
+                src : ['**/*.scss', '!**/_*.scss'],
+                cwd : 'assets/scss',
+                dest : 'assets/css',
+                ext : '.css',
+                expand : true
+              }
+            ],
             options: {
                 style: 'expanded'
             }
         }
-    }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'assets/css',
+          src: '**/*.css',
+          dest: 'assets/css'
+        }]
+      }
+    },
 });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
 
     grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['sass:production', 'autoprefixer']);
 };
