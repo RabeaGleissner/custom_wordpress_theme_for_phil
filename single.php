e147cfbd22ab8794b26df523dbefb42508629e86
Rabea Gleissner <rabeagleissner@gmail.com>
2015-10-05 07:25:43 +0100

    only display tags and categories of post if there are any

--- single.php
+++ single.php
@@ -5,23 +5,20 @@
 </nav>
 
 <article class="single-post">
-  <!-- <div class="single-post-sidebar"> -->
    <!-- <?php# $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'large' ) ?>
     <img src="<?php #echo $image[0] ?>" alt="photo of <?php the_title_attribute() ?>">-->
-
-
     <div class="single-post-categories">
       <?php
-        $categories = get_categories();
-        if ( !empty( $categories ) ) {
-          echo '<h4>Category:</h4>
-          <p><ul>' . the_category() . '</ul></p>';
+        if ( has_category() ) {
+          echo '<h4>Category:</h4>';
+          echo the_category();
+        }
+        if ( has_tag() ) {
+          echo '<h4>Tags:</h4>';
+          echo the_tags( '', ', ' );
         }
       ?>
-      <h4>Tags:</h4>
-      <p><?php the_tags( '', ', ' ) ?></p>
     </div>
-  <!-- </div> -->
   <div class="single-post-content">
     <header class="post-header">
       <h1 class="post-title"><?php the_title() ?></h1>
