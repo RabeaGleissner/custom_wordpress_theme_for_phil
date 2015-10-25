7acf7c7373fbf79c921add33f57814084c30dd4b
Rabea Gleissner <rabeagleissner@gmail.com>
2015-10-25 19:52:37 +0000

    adds social media links to footer

--- footer.php
+++ footer.php
@@ -1,11 +1,27 @@
   </main>
 </div> <!-- closing page-wrapper -->
   <footer class="site-footer" id='footer'>
-   <p>Philip Gleissner copyright &copy; <?php echo date('Y') ?></p>
+   <p>You can also find me here:</p>
+  <ul>
+    <li>
+     <a target="_blank" href="https://twitter.com/philgleissner">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.png" alt="twitter logo">
+    </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://github.com/philgleissner">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/github.png" alt="github logo">
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://www.linkedin.com/pub/philip-gleissner/88/4b0/417">
+       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin.png" alt="linkedin logo">
+    </a></li>
+  </ul>
   </footer>
   <?php wp_footer(); ?>
     <script type="text/javascript">
-    WebFontConfig = {google: { families: [ 'Arimo:400,700:latin','Baumans::latin' ] }};
+    WebFontConfig = {google: { families: [ 'Arimo:400,700:latin'] }};
     (function() {
         var wf = document.createElement('script');
         wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
